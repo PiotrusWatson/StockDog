a0dcf5b0479ccaccddbdd7686c225ae3e7863069
Piotrus Watson <pwatson@pulsion.co.uk>
2017-06-15 14:11:06 +0100

    1-14

---
+++ app/scripts/directives/stk-stock-table.js
@@ -0,0 +1,46 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name stockDogApp.directive:stkStockTable
+ * @description
+ * # stkStockTable
+ */
+angular.module('stockDogApp')
+  .directive('stkStockTable', function () {
+    return {
+      template: 'views/templates/stock-table.html',
+      restrict: 'E',
+      // [1] RESTRICT IT TO ENTIRELY WATCHLIST SCOPE - HAVE TO TELL IT ABOUT WATCHLISTS ALL DAY
+      scope: {
+      	watchlist: '='
+      },
+      // [2] MAKE A CONTROLLER WHICH IS HOW WE LET OTHER DIRECTIVES TALK TO US
+      controller: function($scope){
+      	var rows = [];
+
+      	$scope.$watch('showPercent', function(showPercent){
+      		if(showPercent){
+      			_.each(rows, function(row){
+      				row.showPercent = showPercent;
+      			});
+      		}
+      	});
+
+      	this.addRow = function(row){
+      		rows.push(row);
+      	};
+
+      	this.removeRow = function(row){
+      		_.remove(rows, row);
+      	};
+
+      },
+      link: function($scope) {
+        $scope.showPercent = false;
+        $scope.removeStock = function(stock){
+        	$scope.watchlist.removeStock(stock);
+        };
+      }
+    };
+  });
