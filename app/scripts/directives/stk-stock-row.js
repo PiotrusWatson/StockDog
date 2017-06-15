4c7a40d527d9c1f51d138c6a5cfed2d9f04963cb
Piotrus Watson <pwatson@pulsion.co.uk>
2017-06-15 14:29:10 +0100

    1-15

---
+++ app/scripts/directives/stk-stock-row.js
@@ -0,0 +1,53 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name stockDogApp.directive:stkStockRow
+ * @description
+ * # stkStockRow
+ */
+angular.module('stockDogApp')
+  .directive('stkStockRow', function ($timeout, QuoteService) {
+    return {
+    //[1] TREATED AS AN ELEMENT ATTRIBUTE, ALSO REQUIRES stkStockTable controller 
+      restrict: 'A',
+      require: '^stkStockTable',
+      scope: {
+      	stock: '=',
+      	isLast: '='
+      },
+      //[2]
+      link: function ($scope, $element, $attrs, stockTableCtrl) {
+        //[3] LETS GET A TOOLTIP FOR THIS stock-row
+        $element.tooltip({
+        	placement: 'left',
+        	title: $scope.stock.company.name
+        });
+
+        //[4] HEY FROM HERE WE CAN ADD THIS ROW WE EXIST IN TO THE TABLE
+        stockTableCtrl.addRow($scope);
+
+        //[5] AND REGISTER IT WITH THE QUOTESERVICE
+        QuoteService.register($scope.stock);
+
+        //[6] IF WE CALL DESTROY, WE CAN THEN DEREGISTER IT
+        $scope.$on('$destroy', function(){
+        	stockTableCtrl.removeRow($scope);
+        	QuoteService.deregister($scope.stock);
+        });
+
+        //[7] IF LAST STOCK ROW, GET SOME QUOTES UP IN THIS
+        if ($scope.isLast) {
+        	$timeout(QuoteService.fetch);
+        }
+
+        $scope.$watch('stock.shares', function(){
+        	$scope.stock.marketValue = $scope.stock.shares *
+        		$scope.stock.lastPrice;
+        	$scope.stock.dayChange = $scope.stock.shares *
+        		parseFloat($scope.stock.change);
+        	$scope.stock.save();
+        });
+      }
+    };
+  });
